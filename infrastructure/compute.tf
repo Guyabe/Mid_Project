f305355454014292b7014c1f5b3a6aab2ddc0f9c
Assaf Dori <assafdori@Assafs-MacBook-Pro.local>
2024-09-17 22:46:19 +0300

    feat/add userdata file path

--- infrastructure/compute.tf
+++ infrastructure/compute.tf
@@ -1,36 +1,29 @@
 module "ec2_instance" {
-source  = "terraform-aws-modules/ec2-instance/aws"
+    source  = "terraform-aws-modules/ec2-instance/aws"
+    for_each = toset(["one", "two"])
 
-for_each = toset(["one", "two"])
+    name = "trading-instance-${each.key}"
 
-name = "trading-instance-${each.key}"
+    instance_type            = var.instance_type
+    key_name                 = module.key_pair.key_pair_name
+    monitoring               = true
+    vpc_security_group_ids   = [module.sg.security_group_id]
+    subnet_id                = module.vpc.private_subnets[0]
+    associate_public_ip_address = true  # USED FOR TESTING ONLY, DELETE WHEN TESTING IS CONCLUDED
+    user_data                = file("../application/stock-app/userdata.sh")
+    tags                     = var.tags
+  }
 
-instance_type          = var.instance_type
-key_name               = module.key_pair.key_pair_name
-monitoring             = true
-vpc_security_group_ids = [ module.sg.security_group_id ]
-subnet_id              = module.vpc.private_subnets[0]
-associate_public_ip_address = true // USED FOR TESTING ONLY, DELETE WHEN TESTING IS CONCLUDED
+  module "ec2_instance_2" {
+      source  = "terraform-aws-modules/ec2-instance/aws"
 
-user_data = file("../application/stock-app/userdata.sh")
+      name = "trading-monitoring"
 
-
-      tags = var.tags
-    }
-
-    module "ec2_instance_2" {
-        source  = "terraform-aws-modules/ec2-instance/aws"
-
-        name = "trading-monitoring"
-
-        instance_type          = var.instance_type
-        key_name               = module.key_pair.key_pair_name
-        monitoring             = true
-        vpc_security_group_ids = [ module.sg.security_group_id ]
-        subnet_id              = module.vpc.public_subnets[1]
-
-        tags = {
-          Terraform   = "true"
-          Environment = "dev"
-        }
-      }
+      instance_type            = var.instance_type
+      key_name                 = module.key_pair.key_pair_name
+      monitoring               = true
+      vpc_security_group_ids   = [module.sg.security_group_id]
+      subnet_id                = module.vpc.public_subnets[1]
+      user_data                = file("../application/userdata.sh")
+      tags                     = var.tags
+  }

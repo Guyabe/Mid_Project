1f9bb4b54d1c2d340ff8006395a324a460bf9a53
Assaf Dori <assafxdori@gmail.com>
2024-09-18 11:35:16 +0300

    feat/adding alb security group

--- infrastructure/security-groups.tf
+++ infrastructure/security-groups.tf
@@ -8,3 +8,39 @@ module "sg" {
     ingress_cidr_blocks      = var.ingress_cidr_blocks
     ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
   }
+
+
+  module "alb_sg" {
+      source  = "terraform-aws-modules/security-group/aws"
+
+      name   = "alb-sg"
+      description = "ALB Security Group"
+      vpc_id = module.vpc.vpc_id
+
+      ingress_with_cidr_blocks = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          description = "Allow HTTP"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          description = "Allow HTTPS"
+          cidr_blocks = ["0.0.0.0/0"]
+        },
+      ]
+
+      egress_with_cidr_blocks = [
+        {
+          from_port   = 0
+          to_port     = 0
+          protocol    = "-1"
+          description = "Allow all outbound traffic"
+          cidr_blocks = ["10.0.0.0/16"]
+        },
+      ]
+    }
